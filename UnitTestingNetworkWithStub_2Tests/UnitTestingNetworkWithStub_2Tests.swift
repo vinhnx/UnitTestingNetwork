4ed94d30ef353d64e9582563daae42ff75141d58
Vinh Nguyen <vinh.nguyenxuan@2359media.com>
2018-08-19 11:03:12 +0700

    Initial Commit

---
+++ UnitTestingNetworkWithStub_2Tests/UnitTestingNetworkWithStub_2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  UnitTestingNetworkWithStub_2Tests.swift
+//  UnitTestingNetworkWithStub_2Tests
+//
+//  Created by Vinh Nguyen on 19/8/18.
+//  Copyright © 2018 Vinh Nguyen. All rights reserved.
+//
+
+import XCTest
+@testable import UnitTestingNetworkWithStub_2
+
+class UnitTestingNetworkWithStub_2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
